f7d69c99b2c39e6c2f2118b238c6a3e448b27e9a
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-06-14 21:51:12 -0500

    Ejercicio6

--- Ejercicio6/src/Cafetera/Cafetera.java
+++ Ejercicio6/src/Cafetera/Cafetera.java
@@ -1,21 +1,68 @@
 package Cafetera;
 import java.util.Scanner;
 public class Cafetera {
-       private double capacidadMaxima;
-       private double cantidadActual;
+       private double  capacidadMaxima;
+       private  double cantidadActual ;
 
     public Cafetera() {
 
     }
 
     public void llenarCafetera(){
-
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Cantidad Actual: ");
         double cantidadActual = scanner.nextDouble();
-        if(cantidadActual > capacidadMaxima ){
-            System.out.println("¡Se sobrepasa de la capacidad!");
-        }else {
-            capacidadMaxima = cantidadActual;
-        }
+           this.capacidadMaxima = cantidadActual;
+           this.cantidadActual = cantidadActual;
+    }
+    public double ServirTaza(){
+         Scanner scanner = new Scanner(System.in);
+        System.out.println("Indica el tamaño de la taza: ");
+         double llenarTaza = scanner.nextDouble();
+         if(llenarTaza > cantidadActual){
+
+             System.out.println("Es mayor la capacidad: ");
+
+             double resta = llenarTaza - cantidadActual;
+             System.out.println("Se llena con lo que queda: " + resta);
+             this.cantidadActual = cantidadActual - llenarTaza;
+         }else {
+             this.cantidadActual = cantidadActual - llenarTaza;
+
+         }
+         return llenarTaza;
+    }
+         public void vaciar(){
+        Scanner scanner = new Scanner(System.in);
+             System.out.println("Quieres Vaciar la cafetera S/N");
+             String decision = scanner.nextLine();
+             String mayuscula = decision.toUpperCase();
+             if(mayuscula.equals("S")){
+                 this.cantidadActual = 0;
+             }
+         }
+         public void agregarCafe(){
+             Scanner scanner = new Scanner(System.in);
+             System.out.println("Quieres Agregar mas cafe S/N");
+             String decision = scanner.nextLine();
+             String mayuscula = decision.toUpperCase();
+             if(mayuscula.equals("S")  ){
+                 System.out.println("Agregar Cafe: ");
+                 int cantidad = scanner.nextInt();
+                 if(cantidad+cantidadActual > capacidadMaxima){
+                     System.out.println("La cantidad se excede");
+                     System.out.println("Cantidad Actual:" +cantidadActual);
+                 }else {
+                     this.cantidadActual = cantidadActual + cantidad;
+                 }
+             }
+         }
+
+    public double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public double getCantidadActual() {
+        return cantidadActual;
     }
 }
--- Ejercicio6/src/Main/Main.java
+++ Ejercicio6/src/Main/Main.java
@@ -4,8 +4,23 @@ import Cafetera.Cafetera;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Cafetera cafetera = new Cafetera();
+        cafetera.llenarCafetera();
 
+        boolean trueOr = true;
+        do {
+            cafetera.vaciar();
+
+            System.out.println("Cantidad Actual: " + cafetera.getCantidadActual());
+
+            if (  cafetera.getCantidadActual() == 0) {
+                trueOr = false;
+            }else if(cafetera.getCantidadActual() < cafetera.ServirTaza()){
+                trueOr = false;
+            }
+            cafetera.agregarCafe();
+        }while(trueOr);
     }
 }
