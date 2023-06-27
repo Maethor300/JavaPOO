d472049e50f2a27984e5114838679ca3e65d9d64
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-06-27 18:30:39 -0500

    Ejercicio 5 extra

---
+++ Ejercicio5extra/src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import MesDelAño.MesDelAño;
+
+public class Main {
+    public static void main(String[] args) {
+        MesDelAño mesDelAño = new MesDelAño();
+        mesDelAño.AdivinarMes();
+    }
+}
---
+++ Ejercicio5extra/src/MesDelAño/MesDelAño.java
@@ -0,0 +1,25 @@
+package MesDelAño;
+import java.util.Scanner;
+public class MesDelAño {
+    private String meses[] = {
+            "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"
+    };
+    private String mesSecreto = meses[9];
+    public void AdivinarMes(){
+        System.out.println("Adivine el mes secreto. Introduzca el nombre del mes en minúsculas:");
+        Scanner scanner = new Scanner(System.in);
+        String mes = scanner.next();
+         do {
+           
+             if(mes.equals(mesSecreto)){
+                 System.out.println("Haz acertado");
+                 break;
+             }else {
+
+                 System.out.println("No ha acertado. Intente adivinarlo introduciendo otro mes:");
+                  mes = scanner.next();
+             }
+         }while(true);
+
+    }
+}
